28c79f967d42356346f41e67ec8b7d541aadb669
immalcolm <malcolm_yam@np.edu.sg>
2021-10-19 23:45:47 +0800

    update to include array for json

--- Modules/DDA/Week01/SimpleJson.cs
+++ Modules/DDA/Week01/SimpleJson.cs
@@ -13,6 +13,7 @@ public class SimpleJson : MonoBehaviour
         Player player = new Player();
         player.position = new Vector3(5, 0, 1);
         player.health = 80;
+        player.weapons = new string[] { "Shotgun", "Debalizer" };
 
         //converting data from an object to a string
         string json = JsonUtility.ToJson(player);
@@ -20,13 +21,15 @@ public class SimpleJson : MonoBehaviour
 
         //parsing back json string to object
         Player loadedPlayer = JsonUtility.FromJson<Player>(json);
-        Debug.LogFormat("\nPlayer Data: Position {0} // Health {1} ", loadedPlayer.position, loadedPlayer.health);
-        //Expected Output: Player Data: Position (5.0,0.0,1.0) // Health 80
+        Debug.LogFormat("\nPlayer Data: Position {0} // Health {1} ",
+            loadedPlayer.position, loadedPlayer.health, loadedPlayer.weapons);
+
     }
 
     private class Player
     {
         public Vector3 position;
         public int health;
+        public string[] weapons;
     }
 }
